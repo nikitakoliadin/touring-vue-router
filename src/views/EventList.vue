cca9ad87ae819356629ded18201f6e4dc6d15571
Nikita Koliadin <qThegamEp@gmail.com>
2022-10-27 15:08:25 +0300

    Lesson 4 start

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,24 +2,23 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+
     <div class="pagination">
       <router-link
         id="page-prev"
         :to="{ name: 'EventList', query: { page: page - 1 } }"
         rel="prev"
         v-if="page !== 1"
+        >&#60; Previous</router-link
       >
-        &#60; Previous
-      </router-link>
 
       <router-link
         id="page-next"
         :to="{ name: 'EventList', query: { page: page + 1 } }"
         rel="next"
-        v-if="hasNexPage"
+        v-if="hasNextPage"
+        >Next &#62;</router-link
       >
-        Next &#62;
-      </router-link>
     </div>
   </div>
 </template>
@@ -55,8 +54,9 @@ export default {
     })
   },
   computed: {
-    hasNexPage() {
-      const totalPages = Math.ceil(this.totalEvents / 2)
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+
       return this.page < totalPages
     }
   }
@@ -69,7 +69,6 @@ export default {
   flex-direction: column;
   align-items: center;
 }
-
 .pagination {
   display: flex;
   width: 290px;
@@ -79,9 +78,11 @@ export default {
   text-decoration: none;
   color: #2c3e50;
 }
+
 #page-prev {
   text-align: left;
 }
+
 #page-next {
   text-align: right;
 }
